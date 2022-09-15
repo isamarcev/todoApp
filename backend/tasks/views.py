79654126c60e6cf2a97c93728ae0a9fab9f53a52
isamarcev <isamarcev1994@gmail.com>
2022-09-15 14:19:38 +0300

    dd

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -36,7 +36,6 @@ class SubTaskViewSet(viewsets.ModelViewSet):
 
     def get_object(self, queryset=None, **kwargs):
         item = self.kwargs.get('pk')
-        print(item)
         return get_object_or_404(SubTask, id=item)
 
     def get_queryset(self):
@@ -47,35 +46,13 @@ class SubTaskViewSet(viewsets.ModelViewSet):
         return queryset
 
 
-# class TaskApiList(APIView):
-#     def get(self, request):
-#         queryset = Task.objects.all()
-#         print(queryset)
-#         # serializer_class = TaskSerializer(queryset, many=True)
-#         return super().get(self, request,)
-
-class TaskApoListView(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def get(self, request, *args, **kwargs):
-        self.queryset = Task.objects.filter(user_id=request.user.id)
-        print(request.user)
-        return super(TaskApoListView, self).get(self, request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        print(request.data['title'], request.POST['color'])
-
-
 class TaskSubUserView(APIView):
     permission_classes = []
     def get(self, request, format=None):
-        print(request.GET)
-        print(request.user.id)
         task = Task.objects.filter(user_id=request.user.id)
         serializer = TaskSerializer(task, many=True)
         return Response(serializer.data)
+
 
 class TaskCreateView(generics.CreateAPIView, generics.DestroyAPIView):
     serializer_class = OnlyTaskSerializer
@@ -88,7 +65,6 @@ class TaskDestroyView(generics.DestroyAPIView):
     serializer_class = OnlyTaskSerializer
 
     def get_queryset(self):
-        print(self.request.DELETE)
         queryset = Task.objects.get(id=self.request.id)
         return queryset
 
