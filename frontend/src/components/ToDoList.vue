1858ccb84e4b0cd0ef6d1847853de083be33e241
isamarcev <isamarcev1994@gmail.com>
2022-09-16 10:10:18 +0300

    rename frontend dir

---
+++ frontend/src/components/ToDoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>added TODO</h3>
+    <to-do-item
+      v-for="post in toDo"
+      :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
+    />
+  </div>
+</template>
+
+<script>
+import ToDoItem from "@/components/ToDoItem";
+export default {
+  components: {ToDoItem},
+  props: {
+    toDo: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
